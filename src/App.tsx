6718b15274ce530c017a33dec6799848b5d43a6e
Dinosauron <satanasatana66613@gmail.com>
2022-11-19 00:08:37 +1000

    component=imput

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Field} from 'react-final-form';
+import {Form, Field, useForm} from 'react-final-form';
 import {ImputAnswer} from './components/ImputAnswer';
 import axios from 'axios';
+import { AnyNsRecord } from 'dns';
 
 function App() {
-  
-  const onSubmit = (value:number) => {
+  const onSubmit = (e:any) => {
     axios({
       method: 'post',
       url: '/?service=t_1',
       data: {
-        a:value
+        a:e.answerField
       }
     });
     };
+
   
+
+
   return (
     <Form
-      onSubmit={onSubmit}
-      render={({handleSubmit}) => (
-          <form className='d-inline-flex form-group' onSubmit={handleSubmit}>
-            <span>2</span>
-            <span>+</span>
-            <Field name="ImputField" component={ImputAnswer} placeholder="answer" />
-            <span>=</span>
-            <span>6</span>
+        onSubmit={onSubmit}
+        render={({handleSubmit}) => (
+          <form  onSubmit={handleSubmit}>
+            <div className='d-inline-flex form-group'>
+              <span>2</span>
+              <span>+</span>
+              <Field name="answerField" component="input" placeholder="answer" />
+              <span>=</span>
+              <span >6</span>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
           </form>
-      )}
-    />
+        )}
+      />
   );
 }
 
--- src/components/ImputAnswer.tsx
+++ src/components/ImputAnswer.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import {useState} from "react";
+import {Form, Field, useFormState} from 'react-final-form';
+import axios from 'axios';
 
 export const ImputAnswer = () =>{
+    const [value, setValue] = useState('');
+    const [valueError, setValueError] = useState(false);
+
+    const validateValue = (e:any) => {
+        setValue(e.target.value)
+        if(!/^\d+$/.test(value) && value!=''){
+            setValueError(false)
+        }else{
+            setValueError(true)
+        }
+    }
+
+    const valueHandler = (e:any) => {
+        if(!valueError){
+            console.log(e.target.value)
+        }
+        
+    }
     return(
-        <input type="text" className="form-control" id="input1"></input>
-    )
+        <div>
+        <input style={{color: !valueError? 'red':'black'}}  value={value} onChange={e => validateValue(e)} onBlur={e => valueHandler(e)} name='value' type="text" className="form-control" placeholder="Введите число"/>
+        </div>
+    );
 
 }

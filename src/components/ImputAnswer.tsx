516039156c408c0218bbe10565af3e8195ddde43
Yeminfu <yeminfu4319@gmail.com>
2022-11-19 15:52:36 +1000

    upd validation

--- src/components/ImputAnswer.tsx
+++ src/components/ImputAnswer.tsx
@@ -1,41 +1,19 @@
-// import React from "react";
-// import { useState } from "react";
 import { Field } from 'react-final-form'
 
-export const ImputAnswer = ({ name }: any) => {
-    // console.log('props', props);
-
-    // const [value, setValue] = useState('');
-    // const [valueError, setValueError] = useState(false);
-
-    // const validateValue = (e: any) => {
-    //     setValue(e.target.value)
-    //     if (!/^\d+$/.test(value) && value != '') {
-    //         setValueError(false)
-    //     } else {
-    //         setValueError(true)
-    //     }
-    // }
-
-    // const valueHandler = (e: any) => {
-    //     if (!valueError) {
-    //         console.log(e.target.value)
-    //     }
-
-    // }
+export const ImputAnswer = (props: any) => {
     return (
-        <Field name={name}>{
-            props => (
-                <input
-                    // style={{ color: !valueError ? 'red' : 'black' }}
-                    // value={value}
-                    // onChange={e => validateValue(e)} 
-                    {...props.input}
-                    // onBlur={e => valueHandler(e)}
-                    // name='value'
-                    type="text" className="form-control" placeholder="Введите число" />
-            )}
+        <Field name={props.input.name}
+            validate={value => {
+                if (!/^\d+$/.test(value) || value === '') return 'Required';
+                return undefined;
+            }}
+        >{
+                props => (
+                    <input
+                        style={{ color: props.meta.error ? 'red' : 'black' }}
+                        {...props.input}
+                        type="text" className="form-control" placeholder="Введите число" />
+                )}
         </Field>
     );
-
 }

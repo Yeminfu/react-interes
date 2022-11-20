5df9bb31f7f6d55c71056d9581d961512fc482ad
Yeminfu <yeminfu4319@gmail.com>
2022-11-20 10:13:29 +1000

    up form

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,37 @@
 // import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Field} from 'react-final-form';
-import {ImputAnswer} from './components/ImputAnswer';
+import { Form, Field } from 'react-final-form';
+// import { ImputAnswer } from './components/ImputAnswer';
 import axios from 'axios';
 
 function App() {
-  const onSubmit = (e:any) => {
+  const onSubmit = (e: any) => {
     axios({
       method: 'post',
       url: '/?service=t_1',
       data: {
-        a:e.answerField
+        a: e.answerField
       }
     });
-    };
-
-  
-
+  };
 
   return (
     <Form
-        onSubmit={onSubmit}
-        render={({handleSubmit}) => (
-          <form  onSubmit={handleSubmit}>
-            <div className='d-inline-flex form-group'>
-              <span>2</span>
-              <span>+</span>
-              {/* <ImputAnswer/> */}
-              <Field name="answerField" component={ImputAnswer} placeholder="answer" /> 
-              <span>=</span>
-              <span >6</span>
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </div>
-          </form>
-        )}
-      />
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div className='d-inline-flex align-items-center'>
+            <span className='display-6 me-2'>2</span>
+            <span className='display-6 me-2'>+</span>
+            <Field className="me-2" name="answerField" component={'input'} placeholder="X" style={{ width: "30px" }} />
+            <span className='display-6 me-2'>=</span>
+            <span className='display-6 me-2'>6</span>
+            <button type="submit" className="btn btn-outline-dark btn-sm ms-2">OK</button>
+          </div>
+        </form>
+      )}
+    />
   );
 }
 
